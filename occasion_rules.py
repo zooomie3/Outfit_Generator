6cddf4f9cefb2ec12691731ad4f54812dba8ba59
LiskeJ <bijouxliske@gmail.com>
2026-02-24 15:02:28 +0100

    Update occasion rules with new items and accessories

--- occasion_rules.py
+++ occasion_rules.py
@@ -1,15 +1,15 @@
 # Identifying occasions and giving them output
 occasion_rules = {
-    "night_out": ["BOT_02", "TOP_07", "TOP_05", "SHO_04", "SHO_06", "small bag", "BOT_10", "TOP_09", "OUT_02", "TOP_14", "OUT_01"],
+    "night_out": ["BOT_02", "TOP_07", "TOP_05", "SHO_04", "SHO_06", "BAG_01", "BOT_10", "TOP_09", "OUT_02", "TOP_14", "OUT_01"],
     "family dinner": ["TOP_12", "BOT_10", "TOP_11", "TOP_03", "TOP_04", "TOP_09", "BOT_03", "SHO_03", "OUT_01"],
-    "casual": ["BOT_10", "SHO_03", "TOP_02", "TOP_06", "TOP_13", "TOP_12", "TOP_11", "BOT_11", "TOP_08", "TOP_14", "SHO_07", "SHO_06"],
-    "gym": ["TOP_02", "TOP_13", "BOT_05", "BOT_08", "BOT_09", "SHO_03", "running shoes"], 
-    "formal": ["BOT_12", "blazer", "OUT_04", "TOP_10", "tie", "BOT_03", "SHO_04", "SHO_05", "SHO_06"],
-    "festival": ["cowboy hat", "leather jacket", "cowboy boots", "bandana", "OUT_03", "funky/colorful tops", "BOT_06", "BOT_11", "BOT_04", "SHO_02", "SHO_03", "SHO_06", "ACC_02"], #add flared jeans!!
-    "beach": ["TOP_01", "BOT_01", "BOT_06", "TOP_05", "SHO_01", "SHO_02", "ACC_02", "ACC_03"]
+    "casual": ["BOT_10", "SHO_03", "TOP_02", "TOP_06", "TOP_13", "TOP_12", "TOP_11", "BOT_11", "TOP_08", "TOP_14", "SHO_07", "SHO_06", "BAG_03", "BAG_06"],
+    "gym": ["TOP_02", "TOP_13", "BOT_05", "BOT_08", "BOT_09", "SHO_03", "SHO_08", "BAG_02"], 
+    "formal": ["BOT_12", "LAY_06", "OUT_04", "TOP_10", "BOT_03", "SHO_04", "SHO_05", "SHO_06", "BAG_04", "ACC_04"],
+    "festival": ["HAT_06", "OUT_06", "SHO_09", "HAT_05", "OUT_03", "TOP_06", "TOP_07", "TOP_09", "BOT_06", "BOT_11", "BOT_04", "BOT_12", "SHO_02", "SHO_03", "SHO_06", "ACC_02", "BAG_06"], 
+    "beach": ["TOP_01", "BOT_01", "BOT_06", "TOP_05", "SHO_01", "SHO_02", "ACC_02", "ACC_03", "BAG_05"]
 }
 
-# add some accessoiries later
 # add if rule for raincoat and rain boots etc.
 # add outerwear later as well
-# add flared jeans
+# add rule for sunglasses
+# add accessoiries in excel file/csv
