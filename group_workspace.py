0c89748bb69280fc3af44471dc207af9eac08598
zooomie3 <zosia.mielech@gmail.com>
2026-02-27 13:31:27 +0100

    Update group_workspace.py

--- group_workspace.py
+++ group_workspace.py
@@ -1,4 +1,4 @@
-#Closet
+#CLOSET
 #importing all closet data 
 import pandas as pd
 
@@ -11,6 +11,7 @@ class Closet:
 from pathlib import Path 
 import pandas as pd
 
+# DATA LOADER 
 # Temperature Categories (Celsius)
 # List of tuples: (category_name, lower_bound, upper_bound)
 # float("-/+inf") is used to cover all possible temperature values so we don’t need arbitrary minimum/maximum temperatures
@@ -138,3 +139,22 @@ def get_temperature_for_date(date, weather_df):
     # Final fallback is to use first row to prevent any crashes 
     temp_c = float(weather_df.iloc[0]["temp_celsius"])
     return _classify_temperature(temp_c)
+
+# OCCASION_RULES
+    # Identifying occasions and giving them output
+occasion_rules = {
+    "night_out": ["BOT_02", "TOP_07", "TOP_05", "SHO_04", "SHO_06", "BAG_01", "BOT_10", "TOP_09", "OUT_02", "TOP_14", "OUT_01"],
+    "family dinner": ["TOP_12", "BOT_10", "TOP_11", "TOP_03", "TOP_04", "TOP_09", "BOT_03", "SHO_03", "OUT_01", "TOP_10", "SHO_10"],
+    "casual": ["BOT_10", "SHO_03", "TOP_02", "TOP_06", "TOP_13", "TOP_12", "TOP_11", "BOT_11", "TOP_08", "TOP_14", "SHO_06", "BAG_03", "BAG_06", "TOP_10", "SHO_10"],
+    "gym": ["TOP_02", "TOP_13", "BOT_05", "BOT_08", "BOT_09", "SHO_03", "SHO_08", "BAG_02", "BOT_07"], 
+    "formal": ["BOT_12", "LAY_06", "OUT_04", "TOP_10", "BOT_03", "SHO_04", "SHO_05", "SHO_06", "BAG_04", "ACC_04"],
+    "festival": ["HAT_06", "OUT_06", "SHO_09", "HAT_05", "OUT_03", "TOP_06", "TOP_07", "TOP_09", "BOT_06", "BOT_11", "BOT_04", "BOT_12", "SHO_02", "SHO_03", "SHO_06", "ACC_02", "BAG_06"], 
+    "beach": ["TOP_01", "BOT_01", "BOT_06", "TOP_05", "SHO_01", "SHO_02", "ACC_02", "ACC_03", "BAG_05"]
+}
+
+# add outerwear later as well
+# add accessoiries in excel file/csv
+# hats are missing!! 1-4
+# layers are missing
+# how are we going to add different outerwear clothes... random?
+
