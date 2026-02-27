bff0210d546334d30fb133f62d655a47110ba349
zooomie3 <zosia.mielech@gmail.com>
2026-02-27 14:07:37 +0100

    Update group_workspace.py

--- group_workspace.py
+++ group_workspace.py
@@ -158,3 +158,25 @@ occasion_rules = {
 # layers are missing
 # how are we going to add different outerwear clothes... random?
 
+# FILTER 
+import pandas as pd
+
+def filter_by_occasion(clothing_df, occasion, occasion_dict):
+    """
+    Removes clothing items that are not valid for the given occasion.
+    Returns a filtered DataFrame.
+    """
+    allowed_items = occasion_dict.get(occasion, [])
+    return clothing_df[clothing_df["item_id"].isin(allowed_items)].copy()
+
+
+def filter_by_temperature(clothing_df, temperature):
+    """
+    Removes clothing items that are not suitable for the given temperature.
+    Returns a filtered DataFrame.
+    """
+    return clothing_df[
+        (clothing_df["min_temp"] <= temperature) &
+        (clothing_df["max_temp"] >= temperature)
+    ].copy()
+copy()
