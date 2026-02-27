d3206dc0e20b4b8d40505f59f07c335c4f8d1a62
aidahebberecht-cloud <aida.hebberecht@gmail.com>
2026-02-27 10:14:20 +0100

    Add Closet class to manage closet items

--- closet.py
+++ closet.py
@@ -1,1 +1,10 @@
+#importing all closet data 
+import pandas as pd
+
+class Closet:
+    def __init__(self, csv_path):
+        self.items = pd.read_csv(csv_path)
+
+    def show_items(self):
+            print(self.items)
 
