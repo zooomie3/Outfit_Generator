f6cfcf881d5e5ceae142863c7eb3bc756f318527
aida.hebberecht-cloud <aida.hebberecht@gmail.com>
2026-03-08 02:09:16 +0100

    documentation messages added to dataset class

--- dataset.py
+++ dataset.py
@@ -4,20 +4,21 @@ import pandas as pd
 
 class DataSet:
     """
-    Creating a parent class that stores the CSV file path andloads the CSV into a dataframe.
-    Both closet and weather data are coming from CSVs, read by pandas, and stored as DataFrames so this logic makes snese for a parent class.
+    Creating a parent class that stores the CSV file path and loads the CSV into a dataframe.
+    Both closet and weather data are coming from CSVs, read by pandas, and stored as DataFrames so this logic makes sense for the parent class.
+    Closet and weather will inheret load_data(), get_data(), file_path, and df
     """
     def __init__(self, file_path):
-        self.file_path = Path(file_path) 
-        self.df = None
+        self.file_path = Path(file_path) # stores file location inside the object
+        self.df = None # because data hasn't been loaded yet
 
-    def load_data(self):
-        """Loads the CSV file into a pandas DataFrame."""
-        self.df = pd.read_csv(self.file_path)
+    def load_data(self): 
+        """Reads the CSV file into a pandas DataFrame."""
+        self.df = pd.read_csv(self.file_path) # stores CSV file in df
         return self.df
 
     def get_data(self):
-        """Returns the DataFrame, loading it first if needed."""
+        """Safe way to acces data, checking if CSV is loaded. Returns the DataFrame, loading it first if needed."""
         if self.df is None:
-            self.load_data()
+            self.load_data() # if CSV not loaded, this loads it anyway
         return self.df
