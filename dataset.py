c973a11d848aae2b0e3d863ef8d53279766f3aca
aida.hebberecht-cloud <aida.hebberecht@gmail.com>
2026-03-08 02:36:45 +0100

    new standardizing added to parent class

--- dataset.py
+++ dataset.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import pandas as pd
+from pathlib import Path
 
 
 class DataSet:
@@ -12,13 +12,31 @@ class DataSet:
         self.file_path = Path(file_path) # stores file location inside the object
         self.df = None # because data hasn't been loaded yet
 
-    def load_data(self): 
-        """Reads the CSV file into a pandas DataFrame."""
-        self.df = pd.read_csv(self.file_path) # stores CSV file in df
+
+    def _standardize_columns(self, df):
+        df.columns = ( 
+            df.columns # accesses list of column names
+            .str.strip() # removes extra white space
+            .str.lower() # makes everything lowercase
+            .str.replace(r"\s+", "_", regex=True) # replace space with underscore
+            .str.replace(r"[^0-9a-zA-Z_]+", "_", regex=True) # remove random characters
+            .str.strip("_") # removes extra underscores
+        )
+        return df # returns dataframe with cleaned columns
+
+    def load_data(self):
+        """
+        Reads CSV into DataFrame
+        """
+        df = pd.read_csv(self.file_path) # loads file
+        df = self._standardize_columns(df) # cleans file
+        self.df = df.reset_index(drop=True) # resets row numbers after cleaning (if rows wwere deleted)
         return self.df
 
     def get_data(self):
-        """Safe way to acces data, checking if CSV is loaded. Returns the DataFrame, loading it first if needed."""
+        """
+        Makes sure CSV is loaded into DataFrame and returns it if not
+        """
         if self.df is None:
             self.load_data() # if CSV not loaded, this loads it anyway
         return self.df
